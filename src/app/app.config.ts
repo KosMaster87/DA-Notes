8e69afaca0dc375e0647c35c1c8259ba1eda9917
Konstantin Aksenov <kav.pbg@gmail.com>
2026-08-22 11:08:22 -0300

    chore: add pnpm/ESLint/Prettier/Renovate tooling, CI workflow and lint/build fixes

---
+++ src/app/app.config.ts
@@ -0,0 +1,25 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'devremote2k',
+        appId: '1:421260235007:web:42ba4978b4734d1c2a7cca',
+        databaseURL: 'https://devremote2k-default-rtdb.firebaseio.com',
+        storageBucket: 'devremote2k.appspot.com',
+        apiKey: 'AIzaSyCytj0wRLznDyTrXUaC_R2JVHxnCuUTrHU',
+        authDomain: 'devremote2k.firebaseapp.com',
+        messagingSenderId: '421260235007',
+        measurementId: 'G-6SG08DDZX9',
+      }),
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
+};
---
+++ src/app/firebase-services/note-list.service.ts
@@ -0,0 +1,228 @@
+import { inject, Injectable, OnDestroy } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+
+import {
+  query,
+  where,
+  limit,
+  Firestore,
+  collection,
+  doc,
+  getDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  onSnapshot,
+} from '@angular/fire/firestore';
+// import { Console } from 'console';
+// import { log } from 'console';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NoteListService implements OnDestroy {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+  normalNotes_Marked: Note[] = [];
+
+  unsubTrash;
+  unsubNotes;
+  unsubNotesMarked;
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.unsubNotes = this.subNoteslList();
+    this.unsubNotesMarked = this.subNotesListMarked();
+    this.unsubTrash = this.subTrashList();
+  }
+
+  // ----------------------
+
+  // // Neue Methode um Subkollektionen abzurufen
+  // async getSubcollectionData(parentId: string) {
+  //   const parentDocRef = doc(this.firestore, 'notes', parentId);
+  //   const subColRef = collection(parentDocRef, 'subKos'); // Setze den Namen der Subkollektion
+  //   const subColSnapshot = await getDocs(subColRef);
+  //   subColSnapshot.forEach((doc) => {
+  //     console.log(doc.id, ' => ', doc.data());
+  //   });
+  // }
+
+  // // Methode um alle Subkollektionen rekursiv abzurufen
+  // async getAllSubcollections(parentRef: any) {
+  //   const subColRef = collection(parentRef, 'subKos'); // Setze den Namen der Subkollektion
+  //   const subColSnapshot = await getDocs(subColRef);
+
+  //   subColSnapshot.forEach((doc) => {
+  //     console.log(doc.id, ' => ', doc.data());
+  //     // Rekursion für weitere Subkollektionen
+  //     this.getAllSubcollections(doc.ref);
+  //   });
+  // }
+
+  // // ----------------------
+
+  // loadSubcollection(parentId: string) {
+  //   this.noteListService
+  //     .getSubcollectionData(parentId)
+  //     .then(() => {
+  //       console.log('Subkollektionen wurden geladen.');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Fehler beim Laden der Subkollektionen: ', error);
+  //     });
+  // }
+
+  // loadAllSubcollections(parentId: string) {
+  //   const parentDocRef = this.noteListService.getSingelDocRef(
+  //     'notes',
+  //     parentId
+  //   );
+  //   this.noteListService
+  //     .getAllSubcollections(parentDocRef)
+  //     .then(() => {
+  //       console.log('Alle Subkollektionen wurden geladen.');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Fehler beim Laden aller Subkollektionen: ', error);
+  //     });
+  // }
+
+  subNoteslList() {
+    // let ref = collection(this.firestore, '/notes/6lzWqfosKjR2OupdBWP1/subKos/');
+
+    const notesQuery = query(
+      // ref,
+      this.getNotesRef(),
+      // where('marked', '==', false),
+      // orderBy('title'),
+      // limit(100)
+    );
+    return onSnapshot(
+      notesQuery,
+      (list) => {
+        this.normalNotes = [];
+        list.forEach((element) => {
+          this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+        });
+
+        // list.docChanges().forEach((change) => {
+        //   if (change.type === 'added') {
+        //     console.log('New note: ', change.doc.data());
+        //   }
+        //   if (change.type === 'modified') {
+        //     console.log('Modified note: ', change.doc.data());
+        //   }
+        //   if (change.type === 'removed') {
+        //     console.log('Removed note: ', change.doc.data());
+        //   }
+        // });
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
+  }
+
+  subNotesListMarked() {
+    const notesQuery = query(this.getNotesRef(), where('marked', '==', true), limit(100));
+    return onSnapshot(notesQuery, (list) => {
+      this.normalNotes_Marked = [];
+      list.forEach((element) => {
+        this.normalNotes_Marked.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach((element) => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  async addNote(item: Note, colId: 'notes' | 'trash') {
+    if (colId == 'notes') {
+      await addDoc(this.getNotesRef(), item)
+        .catch((err) => {
+          console.error(err);
+        })
+        .then((docRef) => {
+          console.log('Document written with ID: ', docRef);
+        });
+    } else {
+      await addDoc(this.getTrashRef(), item);
+    }
+  }
+
+  async updateNote(note: Note) {
+    const colId = this.getColIdFromNote(note);
+    if (note.id && colId) {
+      const docRef = this.getSingelDocRef(colId, note.id);
+
+      const docSnapshot = await getDoc(docRef);
+      if (docSnapshot.exists()) {
+        await updateDoc(docRef, this.getCleanJson(note)).catch((err) => {
+          console.error('Fehler beim Aktualisieren des Dokuments: ', err);
+        });
+      } else {
+        console.error(`Dokument mit ID ${note.id} in Sammlung ${colId} existiert nicht`);
+      }
+    }
+  }
+
+  async deleteNote(colId: 'notes' | 'trash', docId: string) {
+    await deleteDoc(this.getSingelDocRef(colId, docId)).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subNoteslList();
+    this.subTrashList();
+    this.subNotesListMarked();
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingelDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
+
+  setNoteObject(obj: Partial<Note>, id: string): Note {
+    return {
+      id: id,
+      type: obj.type || 'note',
+      title: obj.title || '',
+      description: obj.description || '',
+      marked: obj.marked || false,
+    };
+  }
+
+  getColIdFromNote(note: Note): string {
+    if (note.type == 'note') {
+      return 'notes';
+    }
+    return 'trash';
+    // return 'trash';
+    // return 'defaultCollection';
+  }
+
+  getCleanJson(note: Note): Partial<Note> {
+    return {
+      type: note.type,
+      title: note.title,
+      description: note.description,
+      marked: note.marked,
+    };
+  }
+}
